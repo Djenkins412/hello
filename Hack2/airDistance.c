3031124b1bd61ba912159f86557beff06a7711f5
Djenkins412 <djenkins8@huskers.unl.edu>
2022-09-03 20:01:30 +0000

    Sat, Sep 3, 2022, 8:01 PM +00:00

---
+++ Hack2/airDistance.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+
+    double Alat;
+    double Blat;
+    double Along;
+    double Blong;
+    double Dis;
+    double R;
+
+    R = 6,371;
+
+
+
+    printf("Origin:     ");
+    scanf("%lf", &Alat);
+    printf(", ");
+    scanf("%lf", &Along);
+
+    printf("Destination: ");
+    scanf("%lf", &Blat);
+    printf(", ");
+    scanf("%lf", &Blong);
+
+    Dis = arccos(sin(Alat)*sin(Blat)+cos(Alat)*cos(Blat)*cos(Blong - Along))*R ;
+
+    printf("Air distance is %lf kms\n", Dis);
+
+    return 0;
+}
