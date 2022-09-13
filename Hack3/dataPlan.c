0e3c060072cd7f9316e1c607eeaa4b218fd11b12
Djenkins412 <djenkins8@huskers.unl.edu>
2022-09-13 04:18:26 +0000

    Tue, Sep 13, 2022, 4:18 AM +00:00

--- Hack3/dataPlan.c
+++ Hack3/dataPlan.c
@@ -1,2 +1,13 @@
 /** Author: Dezmond Jenkins
- * 
+ *
+ *
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(int argc, char **argv){
