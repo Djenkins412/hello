c0e6a3d9a2b3f6c3241f04a05ad9f462f99655d7
Djenkins412 <djenkins8@huskers.unl.edu>
2022-09-20 12:55:29 +0000

    Tue, Sep 20, 2022, 12:55 PM +00:00

---
+++ Hack5/utils.h
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(int argc, char **argv){
+
+    double degree;
+    double degreesToRadians(double (degree*M_PI)/180);
